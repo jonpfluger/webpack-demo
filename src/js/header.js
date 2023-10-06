8f4b927e6adddc0bf4e244ad382390b3402c137a
jonpfluger <jonpfluger718@gmail.com>
2023-10-05 20:05:43 -0500

    modularized client js

---
+++ src/js/header.js
@@ -0,0 +1,10 @@
+import { headerEl } from "./elements"
+
+export function changeHeaderColor() {
+  const currColor = headerEl.style.backgroundColor
+  if (currColor === 'green') {
+    headerEl.style.backgroundColor = 'red'
+  } else {
+    headerEl.style.backgroundColor = 'green'
+  }
+}
